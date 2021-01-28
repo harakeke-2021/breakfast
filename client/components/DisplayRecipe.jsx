6d3fd338f60b961d4101483ad851e4341572a6ff
Peter <peter.lim91@gmail.com>
2021-01-29 11:21:48 +1300

    complete display recipe mvp

--- client/components/DisplayRecipe.jsx
+++ client/components/DisplayRecipe.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import recipes from '../../data/recipes.mjs'
 
-export default function DisplayRecipe () {
+export default function DisplayRecipe(props) {
+  const id = props.match.params.id
+  const recipe = recipes.find((recipe) => {
+    return recipe.id === parseInt(id)
+  })
   return (
-    <h1>Hello!</h1>
+    <div>
+      <h2>{recipe.name}</h2>
+      <img src={recipe.img} alt={recipe.name}></img>
+      <h3>{recipe.description}</h3>
+      <h4>Ingredients</h4>
+      <ul>
+        {recipe.ingredients.map(ingredient => {
+          return (
+            <li>{ingredient[0].toUpperCase() + ingredient.slice(1)}</li>
+          )
+        })}
+      </ul>
+      <p>{recipe.steps}</p>
+    </div>
   )
 }
