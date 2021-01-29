fb15e11b5ffcad2e6c64b05b8a0ebd19a6c8fc69
Peter <peter.lim91@gmail.com>
2021-01-29 14:19:01 +1300

    add ingredientsboard/linklist toggle

--- client/components/FoodSelector.jsx
+++ client/components/FoodSelector.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
+import ingredients from "../../data/ingredients.mjs"
+import IngredientBoard from "./IngredientBoard"
+import LinkList from "./LinkList"
+import recipeData from "../../data/recipes.mjs"
 
-// import IngredientBoard from './IngredientBoard'
+const ingredientsFilterInitial = ingredients.map((ingredient) => ({
+  name: ingredient,
+  selected: false,
+}))
 
-import ingredients from '../../data/ingredients.mjs'
-import IngredientBoard from './IngredientBoard'
+export default function FoodSelector() {
+  const [ingredientsFilter, setIngredientsFilter] = useState(
+    ingredientsFilterInitial
+  )
 
-// import LinkList from './LinkList'
-import LinkList from './LinkList'
+  const [showIngredientsBoard, toggleIngredientsBoard] = useState(true)
 
-// import recipes from '../../data/recipes.mjs'
-import recipeData from '../../data/recipes.mjs'
-
-const ingredientsFilterInitial = ingredients.map(ingredient => ({ name: ingredient, selected: false }))
-
-export default function FoodSelector () {
-  const [ingredientsFilter, setIngredientsFilter] = useState(ingredientsFilterInitial)
+  const handleClick = () => {
+    const ingredientsFilterLength = ingredientsFilter.filter((ingredient) => {
+      return ingredient.selected === true
+    }).length
+    if (ingredientsFilterLength) {
+      toggleIngredientsBoard(!showIngredientsBoard)
+    }
+  }
 
   return (
     <>
-      <IngredientBoard
-        ingredientsFilter={ingredientsFilter}
-        setIngredientsFilter={setIngredientsFilter}
-      />
+      { showIngredientsBoard &&
+        <IngredientBoard
+          ingredientsFilter={ingredientsFilter}
+          setIngredientsFilter={setIngredientsFilter}
+        />
+      }
       <h3>Recipe List</h3>
+      <button onClick={handleClick}>{showIngredientsBoard ? 'Show Recipes' : 'Show Ingredients Board'}</button>
+      { !showIngredientsBoard &&
       <LinkList recipies={recipeData} />
+      }
     </>
   )
 }
